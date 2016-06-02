872389d505910f47f479030ca16bc55722877e36
huangbiao <19941222hb@gmail.com>
2016-06-02 22:41:50 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+
+from setuptools import setup,find_packages
+
+setup(
+	name="tornaqiniu",
+	version="1.0",
+	author="HuangBiao",
+	author_email="19941222hb@gmail.com",
+	description="An asynchronous qiniu cloud storage client for tornado",
+	license="MIT",
+	packages=find_packages(),
+	include_package_data=True,
+)
