345b7428cfd85bee5144a0caa1d406fffdf86ab1
huangbiao <19941222hb@gmail.com>
2016-06-06 00:12:12 +0800

    add policy operation method

--- tornaqiniu/bucket.py
+++ tornaqiniu/bucket.py
@@ -39,6 +39,76 @@ class Bucket(object):
 	def resource(self,res_key):
 		self._res_instance=Resource(res_key,self)
 		return self._res_instance
+	def upload_token(self,bucket=None,key=None,expires=3600,policys=None):
+		bucket=bucket or self._bucket
+		assert bucket!=None and bucket!="","invalid bucket"
+		if isinstance(policys,dict):
+			self.add_policys(policys)
+		return self._auth.upload_token(bucket,key,expires,self._resource_loader.policys)
+	def add_policy(self,field,value):
+		""" add one policy
+		Args:
+			field:policy name,
+			value:policy value,
+		Returns:
+			None
+		"""
+		self._resource_loader.add_policy(field,value)
+	def add_policys(self,policy_pairs):
+		""" add multi policys
+		Args:
+			policy_pairs:dict type
+		Returns:
+			None
+		"""
+		self._resource_loader.add_policys(policy_pairs)
+	def delete_policy(self,field):
+		""" delete one policy
+		Args:
+			field:policy name
+		Returns:
+			None
+		"""
+		self._resource_loader.delete_policy(field)
+	def delete_policys(self,fields):
+		"""delete multi policys
+		Args:
+			fields:a tuple or list of policy name 
+		Returns:
+			None
+		"""
+		self._resource_loader.delete_policys(fields)
+	def delete_all_policys(self):
+		"""delete all policys
+		"""
+		self._resource_loader.delete_all_policys()
+	def modify_policy(self,field,value):
+		"""modify one policy,if policy not exists ,add it
+		Args:
+			field:policy name
+			value:policy value
+		Returns:
+			None
+		"""
+		self._resource_loader.modify_policy(field,value)
+	def update_policys(self,policy_pairs():
+		"""update all policys
+		Args:
+			policy_pairs:dict type
+		Returns:
+			None
+		"""
+		self._resource_loader.get_flush_policys()
+		self._add_policys(policy_pairs()
+	@property
+	def policys(self):
+		"""get all policys
+		Args:
+			None
+		Returns:
+			all policys
+		"""
+		return self._resource_loader.get_policys()
 	@rq_res 
 	def private_url(self,key,expires=3600,host=None):
 		"""create private resource url
