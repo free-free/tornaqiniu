6cd6836fe0cbeaa4cc723c5f42fd7bd57a564d34
huangbiao <19941222hb@gmail.com>
2016-06-11 00:29:39 +0800

    simplify client code

--- tornaqiniu/client.py
+++ tornaqiniu/client.py
@@ -4,6 +4,7 @@ from tornado.httpclient import AsyncHTTPClient
 import json
 from .resource import *
 from .errors import EncodingError
+from .bucket import Bucket
 from .utils import bytes_encode,bytes_decode,urlsafe_base64_encode,json_encode,json_decode,hmac_sha1
 from .common import Auth
 import base64
@@ -15,5 +16,5 @@ class QiniuClient(object):
 		self._bucket=bucket
 		self._download_host=download_host
 		self._bucket_acp=bucket_acp
-	def bucket(self,bucket_name,bucket_acp=0):
-		Bucket(self._auth,bucket_name,bucket_acp)
+	def bucket(self,bucket_name,host=None,bucket_acp=0):
+		return Bucket(host or self._download_host,self._auth,bucket_name,bucket_acp)
