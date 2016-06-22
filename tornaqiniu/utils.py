e6e221e6a24e5ae336e51912094f852d63398114
huangbiao <19941222hb@gmail.com>
2016-06-22 23:31:15 +0800

    format code

--- tornaqiniu/utils.py
+++ tornaqiniu/utils.py
@@ -3,7 +3,6 @@
 import base64
 import hmac
 import json
-#from .errors import *
 import urllib
 from tornado import gen
 from tornado import httpclient
@@ -12,6 +11,8 @@ import os
 import time
 import random
 import hashlib
+
+
 def json_encode(need_encode):
 	return json.dumps(need_encode)
 
@@ -41,6 +42,7 @@ def hmac_sha1(key,data):
 	bytes_key=bytes_encode(key)
 	bytes_data=bytes_encode(data)
 	return hmac.new(bytes_key,bytes_data,'sha1').digest()
+
 def urlencode(need_encode):
 	return urllib.parse.urlencode(need_encode)
 	
@@ -84,7 +86,6 @@ def send_sync_request(url,headers=None,method="GET",body=None):
 	finally:
 		http_client.close()
 
-
 def  mkdir_recursive(dirname,level=1):
 	if level==1:
 		dirname=os.path.abspath(dirname)
@@ -92,31 +93,28 @@ def  mkdir_recursive(dirname,level=1):
 		mkdir_recursive(os.path.dirname(dirname),level+1)
 	if not os.path.exists(dirname):
 		os.mkdir(dirname)
+
 def multipart_formdata(fields,files):
-	md5=hashlib.md5()
-	md5.update(str(random.random()).encode())
-	boundary='-----'+md5.hexdigest()
-	CRLF=b'\r\n'
-	data=[]
-	for key,value in fields.items():
-		data.append(('--'+boundary).encode())
-		data.append(('Content-Disposition: form-data; name="%s"'%key).encode())
-		data.append(b'')
-		data.append(value.encode())
-	for key,filename in files.items():
-		data.append(('--'+boundary).encode())
-		data.append(('Content-Disposition: form-data; name="%s"; filename="%s"'%(key,filename)).encode())
-		data.append(('Content-Type: application/octet-stream').encode())
-		data.append(("Content-Transfer-Encoding: binary").encode())
-		data.append(b'')
-		with open(filename,"r+b") as f:
-			data.append(f.read())
-	data.append(('--'+boundary+'--').encode())
-	data.append(b'')
-	data=CRLF.join(data)
-	content_type="multipart/form-data; boundary=%s"%boundary
-	return content_type,data
-	
-
-		
-
+    md5=hashlib.md5()
+    md5.update(str(random.random()).encode())
+    boundary='-----'+md5.hexdigest()
+    CRLF=b'\r\n'
+    data=[]
+    for key,value in fields.items():
+        data.append(('--'+boundary).encode())
+        data.append(('Content-Disposition: form-data; name="%s"'%key).encode())
+        data.append(b'')
+        data.append(value.encode())
+    for key,filename in files.items():
+        data.append(('--'+boundary).encode())
+        data.append(('Content-Disposition:form-data; name="%s"; filename="%s"' % (key, filename)).encode())
+        data.append(('Content-Type: application/octet-stream').encode())
+        data.append(("Content-Transfer-Encoding: binary").encode())
+        data.append(b'')
+    with open(filename, "r+b") as f:
+        data.append(f.read())
+    data.append(('--'+boundary+'--').encode())
+    data.append(b'')
+    data = CRLF.join(data)
+    content_type = "multipart/form-data; boundary=%s" % boundary
+    return content_type, data
